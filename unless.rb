56114a122869125fea3215d5c4b8078e0aa78b2a
samuel carlos <samuelcpr464@gmail.com>
2022-01-13 09:28:55 -0300

    adde

---
+++ unless.rb
@@ -0,0 +1,17 @@
+class MiClass 
+
+    def def initialize()
+
+    end 
+
+  def evaluar()
+        cor = "branco"
+     unless cor == "preta"
+        puts "não pode passar"
+     end
+    end
+  end
+
+  clase = MiClass()
+  clase.evaluar()
+  gets()
